21af0929337e7336c9fc138208739efbfc9af5e6
Soumya97 <vadlamannati1997@gmail.com>
2016-12-18 00:04:53 +0530

    Employer mock data and models added

---
+++ src/app/models/employer.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by soumya on 17/12/16.
+ */
+export class Employer{
+  id:number;
+  username:string;
+  companyName:string;
+  location:string;
+  designation:string;
+  industry:string;
+  mobileNumber:number;
+  companyNumber:number;
+}
