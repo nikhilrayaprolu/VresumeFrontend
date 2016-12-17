88e8a0775db9228bb2dfd8fd480c6f0ce452bda8
Soumya97 <vadlamannati1997@gmail.com>
2016-12-17 23:07:23 +0530

    Forms for CV1 page

---
+++ src/app/models/cv1.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by soumya on 17/12/16.
+ */
+export class cv1{
+  id:number;
+  username:string;
+  location:string;
+  wexp:number;
+  profileDesc:string;
+  keySkills:string;
+  industry:string;
+  functional_area:string;
+  arr:string[];
+  fullName:string;
+  mobileNumber:number;
+}
